d41bd86ebaaf616a1490f26892ecce2ab9a0ad3b
Vladimir Fibe <motiw@icloud.com>
2022-01-03 11:12:03 +0600

    расставил объекты интерфейса

--- TConverter/ViewController.swift
+++ TConverter/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var cenliusLabel: UILabel!
+  
+  @IBOutlet weak var slider: UIImageView!
+  @IBOutlet weak var fahrenheitLabel: UILabel!
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
-
+  @IBAction func sliderChanged(_ sender: UISlider) {
+  }
+  
 }
 
